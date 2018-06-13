2d8e63ddcc4e34385930b72cd415d943fec51b27
eranhd <eranm22@gmail.com>
2018-06-13 18:20:50 +0300

    config

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     database: 
-           'mongodb://localhost:27017/'
-        //  'mongodb://eranhd:Gamal2010@ds133127.mlab.com:33127/'
+        //    'mongodb://localhost:27017/'
+         'mongodb://eranhd:Gamal2010@ds133127.mlab.com:33127/'
         //  'mongodb://54.171.146.2:27017'
 }
